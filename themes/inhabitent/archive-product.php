dd12900e795922907ac021ce1670838749cd89b0
johnstonstu <stu.w.johnston@gmail.com>
2018-05-23 16:33:28 -0700

    added shop page and began styling

--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -13,49 +13,56 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
+            <div class="container">
+                    <h1 class="page-tlte">Shop Stuff</h1>
+
+                <?php $terms = get_terms( array (
+                    'taxonomy' => 'product_type',
+                    'hide-empty' => 0,
+                ));
+
+                if (! empty( $terms) && ! is_wp_error( $terms )) : ?>
+                    
+                    <?php foreach($terms as $term) : ?>
+                            <a href="<?php echo get_term_link($term) ?>" class="btn"><?php echo $term ->name; ?></a>
+                <?php endforeach; ?>
+
+                <?php  endif; ?>
+
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-					get_template_part( 'template-parts/content' );
-				?>
 
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
 
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <header class="entry-header">
-                <?php if ( has_post_thumbnail() ) : ?>
-                    <?php the_post_thumbnail( 'large' ); ?>
-                <?php endif; ?>
+                <div class="shop-grid">
+                    <div class="shop-grid-item">
+                        <a href="#">
+                            <div class="thumbnail-wrapper">
+                            <?php if ( has_post_thumbnail() ) : ?>
+                                <?php the_post_thumbnail( 'large' ); ?>
+                            <?php endif; ?>
+                        </div>
+                        <div class="shop-grid-info">
+                            <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+                            <?php get_template_part( 'template-parts/product' );?>
+                            <?php echo CFS()->get( 'price' ); ?>
+                        </div>
+                        </a>
+                    </div>
+                </div>
 
-                <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+                <?php endwhile; ?>
+                <?php else : ?>
 
-                <?php if ( 'post' === get_post_type() ) : ?>
-                <div class="entry-meta">
-                    <?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
-                </div><!-- .entry-meta -->
-                <?php endif; ?>
-            </header><!-- .entry-header -->
 
-            <div class="entry-content">
-                <?php the_excerpt(); ?>
-            </div><!-- .entry-content -->
         </article><!-- #post-## -->
-
 		<?php endif; ?>
+            </div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- themes/inhabitent/inc/extras.php
+++ themes/inhabitent/inc/extras.php
@@ -20,3 +20,33 @@ function red_starter_body_classes( $classes ) {
 	return $classes;
 }
 add_filter( 'body_class', 'red_starter_body_classes' );
+
+function inhabitent_dynamic_css(){
+
+	// make sure about page is loaded
+	if (! is_page_template( 'page-templates/about.php' )){
+		return;
+	}
+
+	//get background image from custom field
+	$image = CFS()->get('hero_image');
+
+	if(! $image){
+		return;
+	}
+
+	//add custom css
+	$hero_css = ".entry-header{ 
+		background: 
+			linear-gradient(to bottom, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0.4) 100%),
+			url({$image}) no-repeat center bottom;
+		height: 100vh;
+		background-size: cover, cover;
+	}";
+	wp_add_inline_style('red-starter-style', $hero_css );
+
+
+
+}
+
+add_action( 'wp_enqueue_scripts', 'inhabitent_dynamic_css');
