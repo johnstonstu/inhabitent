01c88284b48d089079f4dfc80dd32d9f8a150660
johnstonstu <stu.w.johnston@gmail.com>
2018-05-24 13:51:58 -0700

    added links to journal

--- themes/inhabitent/home.php
+++ themes/inhabitent/home.php
@@ -27,28 +27,33 @@ get_header(); ?>
 					?>
 					
 					
-					<?php the_post_thumbnail( 'large' );?>
-					<h2 class="entry-title">
-						<a href="<?php echo the_permalink( post )?>"> <?php the_title();?> </a>
-					</h2>
-					<div class="entry-meta">
-						<span class="posted-on">
-							<?php comments_number( '0 comments', '0 comments', '% comments'); ?>
-						</span>
-						<span class="posted-by">
-							By <?php the_author();?>
-							<?php get_the_date(); ?>
-						</span>
+					<header class="entry-header">
+						<?php the_post_thumbnail( 'large' );?>
+						<?php the_title( sprintf( '<h2 class="entry-tag"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+						<div class="entry-meta">
+							<span class="posted-on">
+								<?php comments_number( '0 comments', '0 comments', '% comments'); ?>
+							</span>
+							<span class="posted-by">
+								By <?php the_author();?>
+								<?php get_the_date(); ?>
+							</span>
+						</div>
+					</header>
+					<div class="entry-content">
+						<p><?php the_content() ?></p>
+						<p>
+							<a href="<?php echo esc_url( get_permalink() ) ?>" class="read-more black-btn">Read More</a>
+						</p>
 					</div>
 
 					<?php endforeach; wp_reset_postdata(); ?>
 
 			</div>
-			<div class="secondary">
-				
+			<div class="secondary widget-area">
+				<?php get_sidebar(); ?>
 			</div>
 		</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
 <?php get_footer(); ?>
--- themes/inhabitent/page.php
+++ themes/inhabitent/page.php
@@ -8,16 +8,23 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
+		<main id="main" class="site-main side-by-side" role="main">
+			<div class="container">
+				<div class="primary">
+					<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
 
+			</div>
+
+			<div class="secondary">
+				<?php get_sidebar(); ?>
+			</div>
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
