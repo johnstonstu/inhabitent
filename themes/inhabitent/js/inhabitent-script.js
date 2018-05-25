7f6b791629fa81b5451e197cf500bb6d6b57c321
johnstonstu <stu.w.johnston@gmail.com>
2018-05-25 13:53:57 -0700

    added search bar to header

--- themes/inhabitent/js/inhabitent-script.js
+++ themes/inhabitent/js/inhabitent-script.js
@@ -1,4 +1,15 @@
-// (function($) {
-//   //js stuff goes here
-//   // $('body').empty('hey');
-// })(jQuery);
+(function($) {
+  //hide and show search field
+  $('.search-submit').click(function() {
+    event.preventDefault();
+    $('.search-field').toggleClass('search-focus');
+  });
+
+  // on enter search
+  $('.search-submit').keypress(function(e) {
+    if (e.which == 13) {
+      $('search-submit').submit();
+      return false;
+    }
+  });
+})(jQuery);
